c22ce97ce3f873c7b0d7547d0c4c57bb82c19fe8
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-01-20 18:03:39 +0200

    Implemented RPA.

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-`pip3 install virtualenv`
+# Template: Basic Python only robot
 
-`python3 -m virtualenv MyEnv`
+Get started with just Python.
 
-`source MyEnv/bin/activate`ta
+This template robot:
+
+- Uses only Python.
+- Provides a simple template to start from (`task.py`).
+
+## Learning materials
+
+- [Python basics](https://robocorp.com/docs/languages-and-frameworks/python)
+- [Best practices in creating Python robots](https://robocorp.com/docs/development-guide/qa-and-best-practices/python-robots)
--- parsing_bot.py
+++ parsing_bot.py
@@ -1,79 +1,73 @@
 import os
-import os.path
 import time
-from typing import Tuple, List, Any, Dict
+from typing import Any, Dict, List, Tuple
 
 import PyPDF4
-import openpyxl
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+from RPA.Browser.Selenium import Selenium
+from RPA.Excel.Files import Files
+from RPA.Tables import Tables
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
 
-from main import output_folder
+browser_lib = Selenium()
+output_folder = os.getcwd() +'/output'
+browser_lib.set_download_directory(output_folder)
+# browser_lib.
 
-chrome_driver_path = "driver/chromedriver"
-
-
-def set_up(dir: str):
-    # function to take care of downloading file
-    def enable_download_headless(driver, download_dir):
-        driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-        params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
-        driver.execute("send_command", params)
-
-    # instantiate a chrome options object, so you can set the size and headless preference
-    # some of these chrome options might be unnecessary, but I just used a boilerplate
-    # change the <path_to_download_default_directory> to whatever your default download folder is located
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_experimental_option("detach", True)
-    chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--verbose')
-    chrome_options.add_experimental_option("prefs", {
-        "download.default_directory": "",
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        "safebrowsing_for_trusted_sources_enabled": False,
-        "safebrowsing.enabled": False
-    })
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-software-rasterizer')
-
-    # initialize driver object and change the <path_to_chrome_driver>
-    # depending on your directory where your chromedriver should be
-    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver_path)
-
-    # change the <path_to_place_downloaded_file> to your directory where you would like to place the downloaded file
-    # download_dir = "output"
-    download_dir = dir
-
-    # function to handle setting up headless download
-    enable_download_headless(driver, download_dir)
-    print('Browser driver created.')
-    return driver
+excel = Files()
+tables = Tables()
 
 
-def click_button(driver, xpath: str, time_slip=0) -> None:
-    """
-    Click "DIVE IN" on the homepage to reveal the spend amounts for each agency
-    :param driver: Selenium driver for browser
-    :param xpath: Xpath of element button. Example: '//*[@id="node-23"]/div/div/div/div/div/div/div/a'
-    :param time_slip: Delay before clicking the button
-    :return: None
-    """
-
-    time.sleep(time_slip)
-    try:
-        driver.find_element(By.XPATH, xpath).click()
-        print('The button was pressed.')
-    except Exception as e:
-        print(f'{e}')
+# chrome_driver_path = "driver/chromedriver"
 
 
-def get_departments_amounts(driver, dep_xpath: str, amo_xpath: str) -> list:
+# def set_up(dir: str):
+#     # function to take care of downloading file
+#     def enable_download_headless(driver, download_dir):
+#         driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+#         params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
+#         driver.execute("send_command", params)
+
+#     # instantiate a chrome options object, so you can set the size and headless preference
+#     # some of these chrome options might be unnecessary, but I just used a boilerplate
+#     # change the <path_to_download_default_directory> to whatever your default download folder is located
+#     chrome_options = Options()
+#     chrome_options.add_argument("--headless")
+#     chrome_options.add_argument("--window-size=1920x1080")
+#     chrome_options.add_experimental_option("detach", True)
+#     chrome_options.add_argument("--disable-notifications")
+#     chrome_options.add_argument('--no-sandbox')
+#     chrome_options.add_argument('--verbose')
+#     chrome_options.add_experimental_option("prefs", {
+#         "download.default_directory": "",
+#         "download.prompt_for_download": False,
+#         "download.directory_upgrade": True,
+#         "safebrowsing_for_trusted_sources_enabled": False,
+#         "safebrowsing.enabled": False
+#     })
+#     chrome_options.add_argument('--disable-gpu')
+#     chrome_options.add_argument('--disable-software-rasterizer')
+
+#     # initialize driver object and change the <path_to_chrome_driver>
+#     # depending on your directory where your chromedriver should be
+#     driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver_path)
+
+#     # change the <path_to_place_downloaded_file> to your directory where you would like to place the downloaded file
+#     # download_dir = "output"
+#     download_dir = dir
+
+#     # function to handle setting up headless download
+#     enable_download_headless(driver, download_dir)
+#     print('Browser driver created.')
+#     return driver
+
+
+def open_the_website(url):
+    browser_lib.open_available_browser(url)
+
+
+def get_departments_amounts(dep_xpath: str, amo_xpath: str) -> list:
     """
     Scrapping spend amounts for each agency from a page.
     :param driver: Selenium driver for browser
@@ -87,12 +81,12 @@ def get_departments_amounts(driver, dep_xpath: str, amo_xpath: str) -> list:
     dep_list = []
     amo_list = []
     try:
-        dep_rows = len(driver.find_elements(By.XPATH, dep_xpath))
-        amo_rows = len(driver.find_elements(By.XPATH, amo_xpath))
+        departments = browser_lib.find_elements(dep_xpath)
+        amounts = browser_lib.find_elements(amo_xpath)
+        dep_rows = len(departments)
+        amo_rows = len(amounts)
         print(f'Found {dep_rows} agencies.')
         print(f'Found {amo_rows} amounts')
-        departments = driver.find_elements(By.XPATH, dep_xpath)
-        amounts = driver.find_elements(By.XPATH, amo_xpath)
         for department in departments:
             dep_list.append(department.text)
         for amount in amounts:
@@ -105,7 +99,35 @@ def get_departments_amounts(driver, dep_xpath: str, amo_xpath: str) -> list:
         print(f'{e}')
 
 
-def open_agency_page(driver, agency_name: str, xpath: str) -> None:
+def save_to_xlsx(data, file_name, sheet_name, path=output_folder):
+    print('Recording to file started.')
+    try:
+        os.chdir(path)
+        if os.path.exists(file_name):
+            workbook = excel.open_workbook(file_name)
+        else:
+            workbook = excel.create_workbook(file_name)   
+        if sheet_name in workbook.sheetnames:
+            # worksheet = excel.set_active_worksheet(sheet_name)
+            worksheet = excel.set_active_worksheet(sheet_name)
+        else:
+            worksheet = workbook.create_worksheet(sheet_name)
+        rows = len(data)
+        cols = len(data[0])
+        for row in range(0, rows):
+            for col in range(0, cols):
+                value = data[row][col]
+                workbook.set_cell_value(row + 1, col + 1, value,name=worksheet)
+        excel.save_workbook(file_name)
+        print('Recording to file finished.')
+    except Exception as e:
+        print(e)
+    finally:
+        excel.close_workbook()
+        os.chdir('..')
+
+
+def open_agency_page(agency_name: str, xpath: str) -> None:
     """
     Open agency's page
     :param driver: Selenium driver for browser
@@ -114,20 +136,20 @@ def open_agency_page(driver, agency_name: str, xpath: str) -> None:
     :return: agency's page
     """
     try:
-        agencies = driver.find_elements(By.XPATH, xpath)
+        agencies = browser_lib.find_elements(xpath)
         for agency in agencies:
             if agency_name in agency.text:
                 print(f'Found {agency_name}.')
                 link = agency.get_attribute('href')
                 print(f'{agency_name}: {link}')
-                driver.get(link)
+                open_the_website(link)
                 print('Agency page opened.')
                 break
     except Exception as e:
         print(f'{e}')
 
 
-def scrap_table(driver, rows_xpath: str, cols_xpath: str) -> Tuple[List[List[Any]], List[Any], List[Dict[str, Any]]]:
+def scrap_table(rows_xpath: str, cols_xpath: str) -> Tuple[List[List[Any]], List[Any], List[Dict[str, Any]]]:
     """
     Scrapping data from table
     :param driver: Selenium driver for browser
@@ -139,51 +161,65 @@ def scrap_table(driver, rows_xpath: str, cols_xpath: str) -> Tuple[List[List[Any
     3. a list to check the comparison between links and files
     """
     global dict_for_check
-    time.sleep(10)
+    # time.sleep(15)
     select_xpath = '//*[@id="investments-table-object_length"]/label/select'
     rows_list = []
     links_list = []
     list_for_check = []
     try:
-        print('Started scraping the table.')
-        select = Select(driver.find_element(By.XPATH, select_xpath))
-        # select by visible text
-        select.select_by_visible_text('All')
-        print('Select "All" selected.')
         print('Waiting for the table to load...')
-        time.sleep(20)
-        rows = len(driver.find_elements(By.XPATH, rows_xpath))
-        cols = len(driver.find_elements(By.XPATH, cols_xpath))
-        print(f'Found {rows} lines.')
-        print(f'Found {cols} columns')
-        for row in range(1, rows + 1):
-            row_list = []
-            for col in range(1, cols + 1):
-                value = driver.find_element(By.XPATH,
-                                            '//*[@id="investments-table-object"]/tbody/tr[' + str(row) + ']/td[' + str(
-                                                col) + ']').text
-                row_list.append(value)
-                if col == 1:
-                    dict_for_check = {}
-                    try:
-                        a_xpath = '//*[@id="investments-table-object"]/tbody/tr[' + str(row) + ']/td[1]/a'
-                        tag_a = driver.find_element(By.XPATH, a_xpath)
-                        link = tag_a.get_attribute('href')
-                        links_list.append(link)
-                        print(f'Added link to list: {link}')
-                        uii_for_check = tag_a.text
-                        investment_title = driver.find_element(By.XPATH,
-                                                               '//*[@id="investments-table-object"]/tbody/tr[' + str(
-                                                                   row) + ']/td[3]').text
-                        dict_for_check['investment'] = investment_title
-                        dict_for_check['uii'] = uii_for_check
-                    except Exception as e:
-                        continue
-                    finally:
-                        if dict_for_check != {}:
-                            list_for_check.append(dict_for_check)
+        while browser_lib.is_element_visible(rows_xpath) == False:
+            pass
+        else:
+            print('Started scraping the table.')
+            last_button_1 = browser_lib.find_element('//*[@id="investments-table-object_last"]')
 
-            rows_list.append(row_list)
+            a = last_button_1.get_attribute('data-dt-idx')
+            select = Select(browser_lib.find_element(select_xpath))
+            select.select_by_visible_text('All')
+            print('Select "All" selected.')
+            print('Waiting for the table to load...')
+            flag = True
+            while flag:
+                try:
+                    last_button_1.get_attribute('data-dt-idx')
+                    flag = True
+                except:
+                    flag = False
+            rows = len(browser_lib.find_elements(rows_xpath))
+            cols = len(browser_lib.find_elements(cols_xpath))
+            print(f'Found {rows} lines.')
+            print(f'Found {cols} columns')
+            for row in range(1, rows + 1):
+                row_list = []
+                for col in range(1, cols + 1):
+                    value = browser_lib.find_element(
+                                                '//*[@id="investments-table-object"]/tbody/tr[' + str(row) + ']/td[' + str(
+                                                    col) + ']').text
+                    row_list.append(value)
+                    if col == 1:
+                        dict_for_check = {}
+                        try:
+                            a_xpath = '//*[@id="investments-table-object"]/tbody/tr[' + str(row) + ']/td[1]/a'
+                            tag_a = browser_lib.find_element(a_xpath)
+                            link = tag_a.get_attribute('href')
+                            links_list.append(link)
+                            print(f'Added link to list: {link}')
+                            uii_for_check = tag_a.text
+                            investment_title = browser_lib.find_element(
+                                                                '//*[@id="investments-table-object"]/tbody/tr[' + str(
+                                                                    row) + ']/td[3]').text
+                            dict_for_check['investment'] = investment_title
+                            dict_for_check['uii'] = uii_for_check
+                            dict_for_check['link'] = link
+                        except Exception as e:
+                            continue
+                        finally:
+                            if dict_for_check != {}:
+                                list_for_check.append(dict_for_check)
+
+                rows_list.append(row_list)
+            
         print('Scraping the table finished.')
         return rows_list, links_list, list_for_check
     except Exception as e:
@@ -207,7 +243,7 @@ def latest_download_file(path=output_folder):
         os.chdir('..')
 
 
-def download_file(driver, links: list, list_for_check: list) -> None:
+def download_file(links: list, list_for_check: list) -> None:
     """
     Download files from list
     :param driver: Selenium driver for browser
@@ -220,12 +256,14 @@ def download_file(driver, links: list, list_for_check: list) -> None:
         if len(links) > 0:
             print('Start downloading files.')
             for link in links:
-                driver.get(link)
+                open_the_website(link)
                 download_xpath = '//*[@id="business-case-pdf"]/a'
-                time.sleep(5)
+                time.sleep(3)
                 try:
-                    driver.find_element(By.XPATH, download_xpath).click()
-                    time.sleep(10)
+                    browser_lib.click_button(browser_lib.find_element(download_xpath))
+                    # while browser_lib.find_element('//*[@id="business-case-pdf"]/a').get_attribute('aria-busy') == True:
+                    #     pass
+                    time.sleep(15)
                     file_ends = "crdownload"
                     while "crdownload" == file_ends:
                         time.sleep(1)
@@ -236,17 +274,19 @@ def download_file(driver, links: list, list_for_check: list) -> None:
                             file_ends = "none"
                     downloaded_files.append(newest_file)
                     print(f'File {newest_file} downloaded successfully.')
-                    compare_data(get_data_from_file(newest_file, 0), list_for_check)
+                    data_from_downloaded_file = get_data_from_pdf_file(newest_file, 0)
+                    data_from_downloaded_file['link'] = link
+                    compare_data(data_from_downloaded_file, list_for_check)
                 except Exception as e:
                     print(f'{e}')
         if len(downloaded_files) == len(links):
-            driver.close()
+            browser_lib.close_all_browsers()
             print('File downloads complete.')
     except Exception as e:
         print(e)
 
 
-def get_data_from_file(file: str, num_page: int, path=output_folder) -> Dict[str, Any]:
+def get_data_from_pdf_file(file: str, num_page: int, path=output_folder) -> Dict[str, Any]:
     """
     Get some data from file
     :param path: default "output"
@@ -286,10 +326,10 @@ def compare_data(data_1: dict, data_2: list) -> bool:
     """
     try:
         flag = False
-        dict_from_file_uii = data_1["uii"]
-        dict_2 = next(item for item in data_2 if item["uii"] == dict_from_file_uii)
-        for j in data_1:
-            if j not in dict_2:
+        dict_from_file_link = data_1['link']
+        dict_2 = next(item for item in data_2 if item['link'] == dict_from_file_link)
+        for key in data_1:
+            if key not in dict_2:
                 flag = False
                 print('Link and download not matched.')
                 break
@@ -299,38 +339,3 @@ def compare_data(data_1: dict, data_2: list) -> bool:
         return flag
     except Exception as e:
         print(e)
-
-
-def save_to_xlsx(data: list, file_name: str, sheet_name: str, path=output_folder) -> None:
-    """
-    Save data to file xlsx
-    :param path: path to dir
-    :param data: data to write to a file
-    :param file_name: a file name
-    :param sheet_name: a sheet name
-    :return:
-    """
-    os.chdir(path)
-    print('Recording to file started.')
-    try:
-        if os.path.exists(file_name):
-            # to load the workbook with file name
-            workbook = openpyxl.load_workbook(file_name)
-        else:
-            workbook = openpyxl.Workbook()
-        if sheet_name in workbook.sheetnames:
-            worksheet = workbook[sheet_name]
-        else:
-            worksheet = workbook.create_sheet(sheet_name)
-        rows = len(data)
-        cols = len(data[0])
-        for row in range(0, rows):
-            for col in range(0, cols):
-                value = data[row][col]
-                worksheet.cell(row=row + 1, column=col + 1).value = value
-                workbook.save(file_name)
-        print('Recording to file finished.')
-    except Exception as e:
-        print(e)
-    finally:
-        os.chdir('..')
