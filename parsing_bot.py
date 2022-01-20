d5d966bed8f5a05657ca97f5335d723d45821f9f
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-01-20 18:16:04 +0200

    Implemented RPA_2

--- parsing_bot.py
+++ parsing_bot.py
@@ -6,61 +6,15 @@ import PyPDF4
 from RPA.Browser.Selenium import Selenium
 from RPA.Excel.Files import Files
 from RPA.Tables import Tables
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
+
 from selenium.webdriver.support.select import Select
 
 browser_lib = Selenium()
-output_folder = os.getcwd() +'/output'
+output_folder = os.getcwd() + '/output'
 browser_lib.set_download_directory(output_folder)
-# browser_lib.
 
 excel = Files()
 tables = Tables()
-
-
-# chrome_driver_path = "driver/chromedriver"
-
-
-# def set_up(dir: str):
-#     # function to take care of downloading file
-#     def enable_download_headless(driver, download_dir):
-#         driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-#         params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
-#         driver.execute("send_command", params)
-
-#     # instantiate a chrome options object, so you can set the size and headless preference
-#     # some of these chrome options might be unnecessary, but I just used a boilerplate
-#     # change the <path_to_download_default_directory> to whatever your default download folder is located
-#     chrome_options = Options()
-#     chrome_options.add_argument("--headless")
-#     chrome_options.add_argument("--window-size=1920x1080")
-#     chrome_options.add_experimental_option("detach", True)
-#     chrome_options.add_argument("--disable-notifications")
-#     chrome_options.add_argument('--no-sandbox')
-#     chrome_options.add_argument('--verbose')
-#     chrome_options.add_experimental_option("prefs", {
-#         "download.default_directory": "",
-#         "download.prompt_for_download": False,
-#         "download.directory_upgrade": True,
-#         "safebrowsing_for_trusted_sources_enabled": False,
-#         "safebrowsing.enabled": False
-#     })
-#     chrome_options.add_argument('--disable-gpu')
-#     chrome_options.add_argument('--disable-software-rasterizer')
-
-#     # initialize driver object and change the <path_to_chrome_driver>
-#     # depending on your directory where your chromedriver should be
-#     driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver_path)
-
-#     # change the <path_to_place_downloaded_file> to your directory where you would like to place the downloaded file
-#     # download_dir = "output"
-#     download_dir = dir
-
-#     # function to handle setting up headless download
-#     enable_download_headless(driver, download_dir)
-#     print('Browser driver created.')
-#     return driver
 
 
 def open_the_website(url):
@@ -70,7 +24,6 @@ def open_the_website(url):
 def get_departments_amounts(dep_xpath: str, amo_xpath: str) -> list:
     """
     Scrapping spend amounts for each agency from a page.
-    :param driver: Selenium driver for browser
     :param dep_xpath: Xpath to department's blocks
     :param amo_xpath: Xpath to amount's blocks
     :return: List of departments and amounts.
@@ -99,16 +52,23 @@ def get_departments_amounts(dep_xpath: str, amo_xpath: str) -> list:
         print(f'{e}')
 
 
-def save_to_xlsx(data, file_name, sheet_name, path=output_folder):
+def save_to_xlsx(data: list, file_name: str, sheet_name: str, path=output_folder):
+    """
+    Save same data to xlsx file
+    :param data: data to save
+    :param file_name: file's name
+    :param sheet_name: sheet's name
+    :param path: path to file. Default: to output folder
+    :return:
+    """
     print('Recording to file started.')
     try:
         os.chdir(path)
         if os.path.exists(file_name):
             workbook = excel.open_workbook(file_name)
         else:
-            workbook = excel.create_workbook(file_name)   
+            workbook = excel.create_workbook(file_name)
         if sheet_name in workbook.sheetnames:
-            # worksheet = excel.set_active_worksheet(sheet_name)
             worksheet = excel.set_active_worksheet(sheet_name)
         else:
             worksheet = workbook.create_worksheet(sheet_name)
@@ -117,7 +77,7 @@ def save_to_xlsx(data, file_name, sheet_name, path=output_folder):
         for row in range(0, rows):
             for col in range(0, cols):
                 value = data[row][col]
-                workbook.set_cell_value(row + 1, col + 1, value,name=worksheet)
+                workbook.set_cell_value(row + 1, col + 1, value, name=worksheet)
         excel.save_workbook(file_name)
         print('Recording to file finished.')
     except Exception as e:
@@ -130,7 +90,6 @@ def save_to_xlsx(data, file_name, sheet_name, path=output_folder):
 def open_agency_page(agency_name: str, xpath: str) -> None:
     """
     Open agency's page
-    :param driver: Selenium driver for browser
     :param agency_name: name of agency
     :param xpath: xpath to agency's link
     :return: agency's page
@@ -152,7 +111,6 @@ def open_agency_page(agency_name: str, xpath: str) -> None:
 def scrap_table(rows_xpath: str, cols_xpath: str) -> Tuple[List[List[Any]], List[Any], List[Dict[str, Any]]]:
     """
     Scrapping data from table
-    :param driver: Selenium driver for browser
     :param rows_xpath: xpath of rows
     :param cols_xpath: xpath of cols
     :return: tuple of:
@@ -168,7 +126,7 @@ def scrap_table(rows_xpath: str, cols_xpath: str) -> Tuple[List[List[Any]], List
     list_for_check = []
     try:
         print('Waiting for the table to load...')
-        while browser_lib.is_element_visible(rows_xpath) == False:
+        while not browser_lib.is_element_visible(rows_xpath):
             pass
         else:
             print('Started scraping the table.')
@@ -194,8 +152,8 @@ def scrap_table(rows_xpath: str, cols_xpath: str) -> Tuple[List[List[Any]], List
                 row_list = []
                 for col in range(1, cols + 1):
                     value = browser_lib.find_element(
-                                                '//*[@id="investments-table-object"]/tbody/tr[' + str(row) + ']/td[' + str(
-                                                    col) + ']').text
+                        '//*[@id="investments-table-object"]/tbody/tr[' + str(row) + ']/td[' + str(
+                            col) + ']').text
                     row_list.append(value)
                     if col == 1:
                         dict_for_check = {}
@@ -207,8 +165,8 @@ def scrap_table(rows_xpath: str, cols_xpath: str) -> Tuple[List[List[Any]], List
                             print(f'Added link to list: {link}')
                             uii_for_check = tag_a.text
                             investment_title = browser_lib.find_element(
-                                                                '//*[@id="investments-table-object"]/tbody/tr[' + str(
-                                                                    row) + ']/td[3]').text
+                                '//*[@id="investments-table-object"]/tbody/tr[' + str(
+                                    row) + ']/td[3]').text
                             dict_for_check['investment'] = investment_title
                             dict_for_check['uii'] = uii_for_check
                             dict_for_check['link'] = link
@@ -219,14 +177,14 @@ def scrap_table(rows_xpath: str, cols_xpath: str) -> Tuple[List[List[Any]], List
                                 list_for_check.append(dict_for_check)
 
                 rows_list.append(row_list)
-            
+
         print('Scraping the table finished.')
         return rows_list, links_list, list_for_check
     except Exception as e:
         print(f'{e}')
 
 
-def latest_download_file(path=output_folder):
+def latest_download_file(path: str = output_folder):
     """
     Get the last modified file in the folder
     :param path: path to folder
@@ -246,7 +204,6 @@ def latest_download_file(path=output_folder):
 def download_file(links: list, list_for_check: list) -> None:
     """
     Download files from list
-    :param driver: Selenium driver for browser
     :param links: list of links
     :param list_for_check: list for check download
     :return:
@@ -289,16 +246,14 @@ def download_file(links: list, list_for_check: list) -> None:
 def get_data_from_pdf_file(file: str, num_page: int, path=output_folder) -> Dict[str, Any]:
     """
     Get some data from file
-    :param path: default "output"
-    :param num_page: number of page (start from 0)
     :param file: file name
+    :param num_page: number of page (start from 0)
+    :param path: default "output"
     :return: list of dictionary with some data from files
     """
     text_list = []
     file_dict = {}
 
-    # cur_path = os.path.dirname(__file__)
-    # new_path = os.path.relpath(file, cur_path)
     try:
         os.chdir(path)
         file_obj = open(file, 'rb')
--- tasks.py
+++ tasks.py
@@ -1,6 +1,4 @@
-import time
 from parsing_bot import *
-
 
 url = 'https://itdashboard.gov/'
 button = 'home-dive-in'
@@ -8,7 +6,6 @@ departments_xpath = '//*[@id="agency-tiles-widget"]/div/div[*]/div[*]/div/div/di
 amounts_xpath = '//*[@id="agency-tiles-widget"]/div/div[*]/div[*]/div/div/div/div[*]/a/span[2]'
 button_xpath = '//*[@id="node-23"]/div/div/div/div/div/div/div/a'
 selected_agency = 'National Science Foundation'
-# selected_agency = 'General Services Administration'
 agency_blocks_xpath = '//*[@id="agency-tiles-widget"]/div/div[*]/div[*]/div/div/div/div[1]/a'
 row_xpath = '//*[@id="investments-table-object"]/tbody/tr[*]'
 col_xpath = '//*[@id="investments-table-object"]/tbody/tr[1]/td'
@@ -18,26 +15,20 @@ sheet_name = 'Agencies'
 
 def main():
     try:
-        time_start = time.time()
         print('The bot started working.')
         open_the_website(url)
-        # browser_lib.find_element(button_xpath).click()
         browser_lib.click_button(browser_lib.find_element(button_xpath))
         save_to_xlsx(get_departments_amounts(departments_xpath, amounts_xpath), file_name, sheet_name)
         open_agency_page(selected_agency, agency_blocks_xpath)
         table, download_list, list_for_check = scrap_table(row_xpath, col_xpath)
         save_to_xlsx(table, file_name, selected_agency)
         download_file(download_list, list_for_check)
-        
+
     except Exception as e:
         print(e)
     finally:
         browser_lib.close_all_browsers()
         print('The bot is finished.')
-        time_finish = time.time()
-        time_all = time_finish - time_start
-        print(time_all)
-
 
 
 if __name__ == '__main__':
